f01dde6cd90aa0c9fcf5fe574ce7e2e9aff38a0b
sultanriazkhan <khansultanriaz@gmail.com>
2026-04-02 14:40:26 +0500

    fast api basic

--- day_1.py
+++ day_1.py
@@ -1,1 +1,12 @@
 from fastapi import FastAPI
+app= FastAPI(
+    title="Meri pehli app",
+    description="Yeh meri pehli API hai",
+    version="1.0"
+)
+@app.get("/")
+def age():
+    return {"message": "Hello World" , "status": "Running"}
+@app.get("/health")
+def health_check():
+    return {"status": "healthy", "service": "api"}
